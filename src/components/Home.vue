8cfc805fe76bcc1d7a091ab66ee10de9103ac7ec
yaxu <ya2698@vip.qq.com>
2020-07-09 11:33:01 +0800

    展示页面调试

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<!-- scoped  防止组件样式渲染到别的页面 -->
+<style lang="less" scoped>
+
+</style>
