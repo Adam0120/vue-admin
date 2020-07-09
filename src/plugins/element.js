4895bafc08fe11bcdf2a3b96cd2869efc528a7f8
yaxu <ya2698@vip.qq.com>
2020-07-09 20:24:40 +0800

    主页基本布局完成

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItem } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
 // 挂在弹框
 Vue.prototype.$message = Message
--- src/router/index.js
+++ src/router/index.js
@@ -4,14 +4,28 @@ import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
 Vue.use(VueRouter)
 
-const routes = [
-  { path: '/', redirect: '/login' },
-  { path: '/login', component: Login },
-  { path: '/home', component: Home }
-]
-
 const router = new VueRouter({
-  routes
+  routes: [
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: Login },
+    { path: '/home', component: Home }
+  ]
+})
+
+// 拦截路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // form 代表从那个路径跳转而来
+  // next 是一个函数,表示放行
+  // next() 放行 next('/login') 强制跳转
+
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  // 没有token跳转到登录页面
+  if (!tokenStr) return next('/login')
+  // 有token直接放行
+  next()
 })
 
 export default router
